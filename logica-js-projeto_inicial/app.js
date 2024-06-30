fc32ff5674a27e82deec77d8e9c4f4262e5d26e6
Emerson Oliveira <emerson.520@outlook.com>
2024-06-30 20:24:23 -0300

    atualização final do jogo

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,10 +1,11 @@
 alert("Boas vindas ao jogo do numero secreto!");
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute; 
-let numeroSecreto = 5;
 let tentativas = 1;
 
 while(chute != numeroSecreto){
-        chute = prompt("Escolha um numero entre um 0 e 30"); 
+        chute = prompt(`Escolha um numero entre um 1 e ${numeroMaximo}`); 
         if(chute == numeroSecreto){
             break;            
         } 
@@ -19,9 +20,12 @@ while(chute != numeroSecreto){
             tentativas+=1;
         }
     }
-        if(tentativas > 1){
+        let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa';         
+        alert(`Parabéns voce descobriu o numero secreto que é:  ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
+
+       /* if(tentativas > 1){
         alert(`Parabéns voce descobriu o numero secreto que é:  ${numeroSecreto} com ${tentativas} tentativas`);
         }
         else{
             alert(`Parabéns voce descobriu o numero secreto que é:  ${numeroSecreto} com ${tentativas} tentativa`);
-        }
+        }*/
