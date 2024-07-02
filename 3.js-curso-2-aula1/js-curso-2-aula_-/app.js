6d013c5444d3a5bf3ff2a6c7beac4e2c3b9371cd
Emerson Oliveira <emerson.520@outlook.com>
2024-07-01 23:00:32 -0300

    Desafio: hora da pratica

--- 3.js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ 3.js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,6 +1,5 @@
 let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do número secreto';
-//titulo.innerHTML = 'Bem-vindo ao mundo dev';
+titulo.innerHTML = 'Hora do Desafio';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
@@ -9,4 +8,39 @@ function verificarChute() {
     console.log("O botão foi clicado");
 }
 
+function euAmoJavascript() {
+    alert("Eu amo JS");
+}
 
+function verificarChute() {
+    console.log("O botão foi clicado");
+  }  
+  
+  function euAmoJavascript() {
+    alert("Eu amo JS");
+  } 
+ 
+  function mostrarCidade() {
+    const cidade = prompt("Qual cidade do Brasil você visitou?");
+  
+    if (cidade === null || cidade === "") {
+      alert("Você não digitou nenhuma cidade.");
+      return;
+    }
+  
+    const mensagem = `Estive em ${cidade} e lembrei de você!`;
+    alert(mensagem);
+  }
+
+  function somarNumeros() {
+    const numero1 = parseInt(prompt("Digite o primeiro número:"));
+    const numero2 = parseInt(prompt("Digite o segundo número:"));
+  
+    if (isNaN(numero1) || isNaN(numero2)) {
+      alert("Você deve digitar apenas números.");
+      return;
+    }
+  
+    const soma = numero1 + numero2;
+    alert(`A soma dos números é: ${soma}`);
+  }
